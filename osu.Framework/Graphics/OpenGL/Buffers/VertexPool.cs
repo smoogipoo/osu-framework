190d96c90d9eb9e5de7d04bf4e6a794d8f187e50
smoogipoo <smoogipoo@smgi.me>
2021-06-09 13:46:59 +0900

    Add custom vertex pool for VBOs

---
+++ osu.Framework/Graphics/OpenGL/Buffers/VertexPool.cs
@@ -0,0 +1,23 @@
+// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using System.Buffers;
+using osu.Framework.Graphics.OpenGL.Vertices;
+
+namespace osu.Framework.Graphics.OpenGL.Buffers
+{
+    internal static class VertexPool<T>
+        where T : struct, IEquatable<T>, IVertex
+    {
+        // ReSharper disable once StaticMemberInGenericType
+        private static readonly ArrayPool<byte> pool;
+
+        static VertexPool()
+        {
+            pool = ArrayPool<byte>.Create(2000 * VertexUtils<T>.STRIDE, 1000);
+        }
+
+        public static VertexMemory<T> Rent(int count) => new VertexMemory<T>(pool, count);
+    }
+}
