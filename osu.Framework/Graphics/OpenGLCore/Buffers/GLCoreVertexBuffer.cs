d671c34dc420803a3dd8709c063a4c4a6c64e732
Dan Balasescu <smoogipoo@smgi.me>
2023-01-19 16:09:26 +0900

    Apply masking texture changes

--- osu.Framework/Graphics/OpenGLCore/Buffers/GLCoreVertexBuffer.cs
+++ osu.Framework/Graphics/OpenGLCore/Buffers/GLCoreVertexBuffer.cs
@@ -5,10 +5,13 @@
 
 using System;
 using System.Buffers;
+using System.Collections.Generic;
 using osu.Framework.Development;
 using osu.Framework.Graphics.Rendering;
 using osu.Framework.Graphics.Rendering.Vertices;
+using osu.Framework.Graphics.Shaders;
 using osu.Framework.Statistics;
+using osuTK;
 using osuTK.Graphics.ES30;
 using SixLabors.ImageSharp.Memory;
 
@@ -47,9 +50,13 @@ namespace osu.Framework.Graphics.OpenGLCore.Buffers
         {
             ref var currentVertex = ref getMemory().Span[vertexIndex];
 
-            bool isNewVertex = !currentVertex.Vertex.Equals(vertex) || currentVertex.BackbufferDrawDepth != Renderer.BackbufferDrawDepth;
+            bool isNewVertex = !currentVertex.Vertex.Equals(vertex)
+                               || currentVertex.BackbufferDrawDepth != Renderer.BackbufferDrawDepth;
+
+            int localId = addMaskingInfo(Renderer.CurrentMaskingInfo);
 
             currentVertex.Vertex = vertex;
+            currentVertex.MaskingTexCoord = new Vector2((localId * MASKING_DATA_LENGTH / 4) % MASKING_TEXTURE_WIDTH, (localId * MASKING_DATA_LENGTH / 4) / MASKING_TEXTURE_WIDTH);
             currentVertex.BackbufferDrawDepth = Renderer.BackbufferDrawDepth;
 
             return isNewVertex;
@@ -129,8 +136,137 @@ namespace osu.Framework.Graphics.OpenGLCore.Buffers
             DrawRange(0, Size);
         }
 
+        public const int MASKING_DATA_LENGTH = 64; // Number of floats that fit into a single MaskingInfo.
+        public const int MASKING_DATA_PER_TEXEL = 4; // Number of floats that fit into a single texel of the masking texture.
+        public const int MASKING_TEXTURE_WIDTH = MASKING_DATA_LENGTH / MASKING_DATA_PER_TEXEL; // Width of the masking texture - holds a single MaskingInfo for now.
+
+        protected int MaskingTextureHeight; // Height of the masking texture - expands based on the number of MaskingInfos.
+        protected float[] MaskingTextureBuffer = new float[MASKING_DATA_LENGTH]; // All data stored in the texture.
+
+        private int maskingTexture = -1;
+        private int numMaskingInfos;
+        private int numUploadedMaskingInfos;
+
+        private Dictionary<int, int> localMaskingInfoIndex = new Dictionary<int, int>();
+
+        private int addMaskingInfo(MaskingInfo info)
+        {
+            int localId;
+
+            if (localMaskingInfoIndex.TryGetValue(info.Id, out localId))
+            {
+                return localId;
+            }
+
+            localId = numMaskingInfos++;
+            localMaskingInfoIndex.Add(info.Id, localId);
+
+            // Row of the masking texture.
+            int index = localId * MASKING_DATA_LENGTH;
+
+            // Ensure we have enough space for this masking data by doubling the array size every time.
+            if (index >= MaskingTextureBuffer.Length)
+                Array.Resize(ref MaskingTextureBuffer, Math.Max(MASKING_DATA_LENGTH, MaskingTextureBuffer.Length * 2));
+
+            float realBorderThickness = info.BorderThickness / info.BlendRange;
+
+            int i = index;
+
+            MaskingTextureBuffer[i++] = info.Id == 0 ? 0.0f : 1.0f;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M11;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M12;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M13;
+
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M21;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M22;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M23;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M31;
+
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M32;
+            MaskingTextureBuffer[i++] = info.ToMaskingSpace.M33;
+            MaskingTextureBuffer[i++] = info.CornerRadius;
+            MaskingTextureBuffer[i++] = info.CornerExponent;
+
+            MaskingTextureBuffer[i++] = info.MaskingRect.Left;
+            MaskingTextureBuffer[i++] = info.MaskingRect.Top;
+            MaskingTextureBuffer[i++] = info.MaskingRect.Right;
+            MaskingTextureBuffer[i++] = info.MaskingRect.Bottom;
+
+            MaskingTextureBuffer[i++] = info.BorderColour.TopLeft.Linear.R;
+            MaskingTextureBuffer[i++] = info.BorderColour.TopLeft.Linear.G;
+            MaskingTextureBuffer[i++] = info.BorderColour.TopLeft.Linear.B;
+            MaskingTextureBuffer[i++] = info.BorderColour.TopLeft.Linear.A;
+
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomLeft.Linear.R;
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomLeft.Linear.G;
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomLeft.Linear.B;
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomLeft.Linear.A;
+
+            MaskingTextureBuffer[i++] = info.BorderColour.TopRight.Linear.R;
+            MaskingTextureBuffer[i++] = info.BorderColour.TopRight.Linear.G;
+            MaskingTextureBuffer[i++] = info.BorderColour.TopRight.Linear.B;
+            MaskingTextureBuffer[i++] = info.BorderColour.TopRight.Linear.A;
+
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomRight.Linear.R;
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomRight.Linear.G;
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomRight.Linear.B;
+            MaskingTextureBuffer[i++] = info.BorderColour.BottomRight.Linear.A;
+
+            MaskingTextureBuffer[i++] = realBorderThickness;
+            MaskingTextureBuffer[i++] = info.BlendRange;
+            MaskingTextureBuffer[i++] = info.AlphaExponent;
+            MaskingTextureBuffer[i++] = info.Hollow ? 1 : 0;
+
+            MaskingTextureBuffer[i++] = info.EdgeOffset.X;
+            MaskingTextureBuffer[i++] = info.EdgeOffset.Y;
+            MaskingTextureBuffer[i++] = info.HollowCornerRadius;
+
+            return localId;
+        }
+
+        private void uploadMaskingTexture()
+        {
+            if (MaskingTextureBuffer.Length == 0)
+                return;
+
+            GL.ActiveTexture(TextureUnit.Texture10);
+
+            if (maskingTexture == -1)
+            {
+                maskingTexture = GL.GenTexture();
+                GL.BindTexture(TextureTarget.Texture2D, maskingTexture);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)All.Nearest);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)All.Nearest);
+            }
+
+            GL.BindTexture(TextureTarget.Texture2D, maskingTexture);
+
+            int totalTexels = numMaskingInfos * MASKING_DATA_LENGTH / MASKING_DATA_PER_TEXEL;
+            int currentMaskingTextureHeight = (totalTexels + MASKING_TEXTURE_WIDTH - 1) / MASKING_TEXTURE_WIDTH;
+            // if (MaskingTextureHeight < currentMaskingTextureHeight) {
+            MaskingTextureHeight = currentMaskingTextureHeight;
+            GL.TexImage2D(All.Texture2D, 0, All.Rgba32f, MASKING_TEXTURE_WIDTH, MaskingTextureHeight, 0, All.Rgba, All.Float, ref MaskingTextureBuffer[0]);
+            // } else if (numUploadedMaskingInfos < currentMaskingTextureHeight) {
+            //     GL.TexSubImage2D(All.Texture2D, 0, 0, numUploadedMaskingInfos, MASKING_TEXTURE_WIDTH, currentMaskingTextureHeight - numUploadedMaskingInfos, All.Rgba, All.Float, ref MaskingTextureBuffer[numUploadedMaskingInfos * MASKING_TEXTURE_WIDTH * 4]);
+            // }
+
+            numUploadedMaskingInfos = currentMaskingTextureHeight;
+
+            GL.ActiveTexture(TextureUnit.Texture0);
+        }
+
         public void DrawRange(int startIndex, int endIndex)
         {
+            uploadMaskingTexture();
+
+            // ReSharper disable once PossibleLossOfFraction
+            GlobalPropertyManager.Set(GlobalProperty.MaskingTextureSize, new Vector2(MASKING_TEXTURE_WIDTH, MaskingTextureHeight));
+
+            // Reset masking info state for the next draw.
+            numMaskingInfos = 0;
+            numUploadedMaskingInfos = 0;
+            localMaskingInfoIndex.Clear();
+
             Bind(true);
 
             int countVertices = endIndex - startIndex;
