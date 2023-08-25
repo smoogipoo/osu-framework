e751fbbfbc8f1c37cf4c181eddb8395b20eaf3e2
Dan Balasescu <smoogipoo@smgi.me>
2023-08-25 14:52:45 +0900

    Add fullscreen test

--- SampleGame/SampleGameGame.cs
+++ SampleGame/SampleGameGame.cs
@@ -1,35 +1,112 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using System.Drawing;
 using osu.Framework;
 using osu.Framework.Graphics;
-using osuTK;
-using osuTK.Graphics;
 using osu.Framework.Graphics.Shapes;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Configuration;
+using osu.Framework.Extensions;
+using osu.Framework.Extensions.Color4Extensions;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using osuTK;
 
 namespace SampleGame
 {
     public partial class SampleGameGame : Game
     {
-        private Box box = null!;
+        [Resolved]
+        private FrameworkConfigManager frameworkConfig { get; set; } = null!;
+
+        private Bindable<Size> configWindowSize = null!;
+
+        private SpriteText infoText = null!;
 
         [BackgroundDependencyLoader]
         private void load()
         {
-            Add(box = new Box
+            frameworkConfig.SetValue(FrameworkSetting.WindowMode, WindowMode.Fullscreen);
+
+            Add(new Box
             {
                 Anchor = Anchor.Centre,
                 Origin = Anchor.Centre,
-                Size = new Vector2(150, 150),
-                Colour = Color4.Tomato
+                RelativeSizeAxes = Axes.Both,
+                Alpha = 0.1f
             });
+
+            Dropdown<Size> resolutionDropdown;
+
+            Add(new FillFlowContainer
+            {
+                Anchor = Anchor.TopCentre,
+                Origin = Anchor.TopCentre,
+                Spacing = new Vector2(5),
+                Children = new Drawable[]
+                {
+                    infoText = new SpriteText
+                    {
+                        Anchor = Anchor.TopCentre,
+                        Origin = Anchor.TopCentre,
+                    },
+                    new FillFlowContainer
+                    {
+                        Anchor = Anchor.TopCentre,
+                        Origin = Anchor.TopCentre,
+                        AutoSizeAxes = Axes.Both,
+                        Direction = FillDirection.Horizontal,
+                        Children = new Drawable[]
+                        {
+                            new SpriteText
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Text = "Resolution: "
+                            },
+                            resolutionDropdown = new BasicDropdown<Size>
+                            {
+                                Anchor = Anchor.CentreLeft,
+                                Origin = Anchor.CentreLeft,
+                                Width = 200,
+                                Items = new[]
+                                {
+                                    new Size(1280, 720),
+                                    new Size(1920, 1080)
+                                },
+                            }
+                        }
+                    },
+                    new BasicButton
+                    {
+                        Anchor = Anchor.TopCentre,
+                        Origin = Anchor.TopCentre,
+                        Width = 200,
+                        Height = 30,
+                        Text = "Take screenshot",
+                        FlashColour = FrameworkColour.BlueGreen.Lighten(0.2f),
+                        Action = takeScreenshot
+                    }
+                }
+            });
+
+            configWindowSize = frameworkConfig.GetBindable<Size>(FrameworkSetting.SizeFullscreen);
+            configWindowSize.BindValueChanged(s => resolutionDropdown.Current.Value = s.NewValue, true);
+            resolutionDropdown.Current.BindValueChanged(s => frameworkConfig.SetValue(FrameworkSetting.SizeFullscreen, s.NewValue));
+        }
+
+        private void takeScreenshot()
+        {
+            Host.TakeScreenshotAsync().WaitSafely();
         }
 
         protected override void Update()
         {
             base.Update();
-            box.Rotation += (float)Time.Elapsed / 10;
+            infoText.Text = $"Renderer: {Host.RendererInfo}, Display: {Host.Window.CurrentDisplayMode.Value.ToString()}";
         }
     }
 }
