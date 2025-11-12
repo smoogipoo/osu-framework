d3ab9397d209cddaa1289b3c6abec3ad9a3b8719
Dan Balasescu <smoogipoo@smgi.me>
2025-11-12 21:07:39 +0900

    Remove unused events

--- osu.Framework.SourceGeneration/Generators/GeneratorEventDriver.cs
+++ osu.Framework.SourceGeneration/Generators/GeneratorEventDriver.cs
@@ -2,8 +2,6 @@
 // See the LICENCE file in the repository root for full licence text.
 
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
 
 namespace osu.Framework.SourceGeneration.Generators
 {
@@ -11,9 +9,6 @@ namespace osu.Framework.SourceGeneration.Generators
     {
         public event Action<object>? SyntaxTargetCreated;
         public event Action<object>? SemanticTargetCreated;
-        public event Action<ImmutableArray<object>>? Stage2Entry;
-        public event Action<ImmutableArray<object>>? Stage2GenerationIdAssigned;
-        public event Action<HashSet<object>>? Stage2Exit;
         public event Action<object>? Emit;
 
         public void OnSyntaxTargetCreated(object target)
@@ -24,21 +19,6 @@ namespace osu.Framework.SourceGeneration.Generators
         public void OnSemanticTargetCreated(object target)
         {
             conditionalInvoke(SemanticTargetCreated, target);
-        }
-
-        public void OnStage2Entry(ImmutableArray<object> target)
-        {
-            conditionalInvoke(Stage2Entry, target);
-        }
-
-        public void OnStage2GenerationIdAssigned(ImmutableArray<object> target)
-        {
-            conditionalInvoke(Stage2GenerationIdAssigned, target);
-        }
-
-        public void OnStage2Exit(HashSet<object> target)
-        {
-            conditionalInvoke(Stage2Exit, target);
         }
 
         public void OnEmit(object candidate)
